35373c6f23374e14a8b0b2c72ef12aac3d400be0
Diego <dnpietruszka@gmail.com>
2019-10-26 20:56:51 -0300

    clse Estudiante

--- Obigatorio2/src/obligatorio2/model/Estudiante.java
+++ Obigatorio2/src/obligatorio2/model/Estudiante.java
@@ -11,4 +11,55 @@ package obligatorio2.model;
  */
 public class Estudiante {
     
+    private String nombre;
+    private long CI;
+    private String mail;
+    private int numero;
+    private int semestre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getCI() {
+        return CI;
+    }
+
+    public void setCI(long CI) {
+        this.CI = CI;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    
+    @Override
+    public boolean equals(Object e){
+        return this.CI == ((Estudiante)e).getCI();
+    }
+    
 }
