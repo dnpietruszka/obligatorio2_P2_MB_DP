01bfb48d6bca4cdf2c1355f5943019aef48f54ab
Diego <dnpietruszka@gmail.com>
2019-11-16 20:22:19 -0300

    problema

--- Obigatorio2/src/obligatorio2/model/Problema.java
+++ Obigatorio2/src/obligatorio2/model/Problema.java
@@ -11,4 +11,53 @@ package obligatorio2.model;
  */
 public class Problema {
     
+    
+    private String titulo;
+    private String descripcion;
+    private String link;
+    private Docente docente;
+    
+    public Problema(String titulo, String decripcion, String link, Docente d){
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.link = link;
+        this.docente = d;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Docente getDocente() {
+        return docente;
+    }
+
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
+    
+    @Override
+    public String toString(){
+        return this.getTitulo();
+    }
 }
